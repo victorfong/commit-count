1879598d7613731a1181eb8b34ba95925beb8039
Victor <i.am.victor.fong@gmail.com>
2015-10-18 17:44:34 -0400

    Script to count commits

--- src/commit_count.go
+++ src/commit_count.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
-	"exec"
+	"os/exec"
+	"bufio"
+	"os"
+	"strings"
+	"bytes"
+	"strconv"
 )
 
 type Repository struct {
@@ -28,7 +33,6 @@ func UnmarshalYaml(data []byte) (Setting, error) {
 	if err != nil {
 		return Setting{}, err
 	}
-	fmt.Printf("--- t:\n%v\n\n", t)
 
 	return t, nil
 }
@@ -48,24 +52,91 @@ func ReadSettingFile(file_path string) (Setting, error) {
 	return setting, err
 }
 
+func fetchSource(repo Repository) error{
+	fmt.Printf("Fetching %s\n", repo.Name)
+	
+	var cmd exec.Cmd = exec.Cmd {
+		Path: "../bin/fetch-source",
+		Args: []string {repo.Name, repo.Url},
+	}
+	
+	err := cmd.Run()
+	return err
+}
 
+func CountCommits(file_path string, repo_name string, setting Setting) map[string]int{
+	var result map[string]int = make(map[string]int) 
+	for _, contributor := range setting.Contributors {
+		result[contributor.Name] = 0
+	}
+	
+	inFile, _ := os.Open(file_path)
+	defer inFile.Close()
+	
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		var line string = scanner.Text()
+		for _, contributor := range setting.Contributors {
+			if strings.Contains(line, contributor.Name) {
+				result[contributor.Name]++
+			}
+		}
+	}
+	
+	return result
+} 
 
-//func fetchSource(repo Repository) {
-//	exec.Run(app, []string{app, "-l"}, nil, "", exec.DevNull, exec.Pipe, exec.Pipe)
-//}
+func CreateOutputFile(setting Setting, result map[string]map[string]int) {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(",")
+	for i, repo := range setting.Repositories {
+		if i != 0 {
+			buffer.WriteString(",")	
+		}
+		buffer.WriteString(repo.Name)
+    }
+	buffer.WriteString("\n")
+	
+	for _, contributor := range setting.Contributors {
+		buffer.WriteString(contributor.Name)
+		buffer.WriteString(",")
+		for j, repo := range setting.Repositories {
+			if j != 0 {
+				buffer.WriteString(",")	
+			}
+			buffer.WriteString(strconv.Itoa(result[contributor.Name][repo.Name]))
+		}
+		buffer.WriteString("\n")
+	}
+	fmt.Printf(buffer.String())
+	ioutil.WriteFile("work/result.csv", buffer.Bytes(), 0644)
+
+}
 
 func main() {
 	fmt.Printf("Reading Setting File: setting.yml\n")
 	setting, err := ReadSettingFile("setting.yml")
 	if err != nil { panic(err) }
 	
+	var result map[string]map[string]int = make(map[string]map[string]int)
+	for _, contributor := range setting.Contributors {
+		result[contributor.Name] = make(map[string]int)
+	} 
 	
+	fmt.Printf("Fetching History\n")
+	for _, repo := range setting.Repositories {
+		fetchSource(repo)
+		
+		var file_path string = "work/" + repo.Name + "_log.txt" 
+		var repo_counts map[string]int = CountCommits(file_path, repo.Name, setting)
+		
+		for _, contributor := range setting.Contributors {
+			result[contributor.Name][repo.Name] = repo_counts[contributor.Name]
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
+	CreateOutputFile(setting, result)
+			
 }
