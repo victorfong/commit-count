4689fa3fd65829ff8250fb2859b85622343f2767
Scott Weiss <scott.weiss@emc.com>
2015-10-22 18:09:07 -0400

    Added commit log
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- src/commit_count.go
+++ src/commit_count.go
@@ -48,51 +48,146 @@ func ReadSettingFile(file_path string) (Setting, error) {
 	if err != nil {
 		return Setting{}, err
 	}
-	
+
 	setting, err := UnmarshalYaml(dat)
 	return setting, err
 }
 
 func fetchSource(repo Repository) error{
 	fmt.Printf("Fetching %s\n", repo.Name)
-	
+
 	var cmd *exec.Cmd = exec.Command(
 		"bin/fetch-source",
-		repo.Name, 
+		repo.Name,
 		repo.Url)
-	
-	result, err := cmd.CombinedOutput()
-	fmt.Printf("result = " + string(result))
-	
+
+	_, err := cmd.CombinedOutput()
 	return err
 }
 
-func CountCommits(file_path string, repo_name string, setting Setting) map[string]int{
+type GitCommit struct {
+	Author string
+	Description string
+	CoAuthor string
+	Repo string
+}
+
+func GetCoAuthor(line string) string {
+	elements := strings.Split(strings.Trim(line, " "), " ")
+	result := elements[1] + " " + elements[2]
+	return result
+}
+
+func GetFirstWord(line string) string {
+	elements := strings.Split(strings.Trim(line, " "), " ")
+	return elements[0]
+}
+
+func ReadCommit(scanner *bufio.Scanner, repo string) []GitCommit{
+	var result []GitCommit
+
+	for scanner.Scan() {
+		var line string = scanner.Text()
+
+		// var pair string
+
+		var firstWord string = GetFirstWord(line)
+
+		if firstWord == "Author:" {
+
+			var author string
+			var description string
+			var coauthor string
+
+			isTwoAuthorPattern, author, coauthor := IsTwoAuthorPattern(line)
+			if !isTwoAuthorPattern {
+				author = GetAuthor(line)
+			}
+
+			// Date
+			scanner.Scan()
+			// scanner.Text()
+
+			// Blank line
+			scanner.Scan()
+			// scanner.Text()
+
+			for scanner.Scan() {
+					line = scanner.Text()
+
+					firstWord = GetFirstWord(line)
+					if firstWord == "Signed-off-by:" {
+						coauthor = GetCoAuthor(line)
+
+						break;
+					} else if firstWord == "commit" {
+						break;
+					}
+
+					description += " "
+					description += strings.Trim(line, " ")
+			}
+
+			description = strings.Trim(description, " ")
+
+			commit := GitCommit{
+				Author: author,
+				Description: description,
+				CoAuthor: coauthor,
+				Repo: repo,
+			}
+			result = append(result, commit)
+
+		}
+	}
+
+	return result
+}
+
+func GetAuthor(line string) string {
+	elements := strings.Split(line, " ")
+	result := elements[1] + " " + elements[2]
+	return result
+}
+
+func IsTwoAuthorPattern(line string) (bool, string, string){
+	if strings.Contains(line, " and ") {
+		elements := strings.Split(line, " ")
+		if elements[3] == "and" {
+			author1 := elements[1] + " " + elements[2]
+			author2 := elements[4] + " " + elements[5]
+			return true, author1, author2
+		}
+	}
+	return false, "", ""
+}
+
+func CountCommits(file_path string, repo_name string, setting Setting, log_buffer bytes.Buffer) map[string]int {
 	fmt.Printf("Reading log file: %s\n", file_path)
-	
-	var result map[string]int = make(map[string]int) 
+
+	var result map[string]int = make(map[string]int)
 	for _, contributor := range setting.Contributors {
 		result[contributor.Name] = 0
 	}
-	
+
 	inFile, err := os.Open(file_path)
 	if err != nil { panic(err) }
 	defer inFile.Close()
-	
+
 	scanner := bufio.NewScanner(inFile)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		var line string = scanner.Text()
-		
+
 		for _, contributor := range setting.Contributors {
 			if strings.Contains(line, contributor.Name) {
 				result[contributor.Name]++
 			}
 		}
 	}
-	
+
 	return result
-} 
+}
 
 func CreateOutputFile(setting Setting, result map[string]map[string]int) {
 	var buffer bytes.Buffer
@@ -100,19 +195,20 @@ func CreateOutputFile(setting Setting, result map[string]map[string]int) {
 	buffer.WriteString(",")
 	for i, repo := range setting.Repositories {
 		if i != 0 {
-			buffer.WriteString(",")	
+			buffer.WriteString(",")
 		}
 		buffer.WriteString(repo.Name)
-    }
+  }
 	buffer.WriteString("\n")
-	
+
 	for _, contributor := range setting.Contributors {
 		buffer.WriteString(contributor.Name)
 		buffer.WriteString(",")
 		for j, repo := range setting.Repositories {
 			if j != 0 {
-				buffer.WriteString(",")	
+				buffer.WriteString(",")
 			}
+			fmt.Printf("%s at repo %s = %d\n", contributor.Name, repo.Name, result[contributor.Name][repo.Name])
 			buffer.WriteString(strconv.Itoa(result[contributor.Name][repo.Name]))
 		}
 		buffer.WriteString("\n")
@@ -122,37 +218,94 @@ func CreateOutputFile(setting Setting, result map[string]map[string]int) {
 
 }
 
+func IsEmcCommit(commit GitCommit, contributors []Contributor) (bool, string){
+	for _, contributor := range contributors {
+		if(contributor.Name == commit.Author) {
+			return true, contributor.Name
+		}
+		if(contributor.Name == commit.CoAuthor) {
+			return true, contributor.Name
+		}
+	}
+
+	return false, ""
+}
+
+func CreateLogOutputFile(setting Setting, log_result map[string][]GitCommit) {
+	var buffer bytes.Buffer
+
+	buffer.WriteString("Author,CoAuthor,Code Repo,Commit Description\n")
+	for _, contributor := range setting.Contributors {
+		var gitCommits []GitCommit = log_result[contributor.Name]
+		for _, commit := range gitCommits {
+			buffer.WriteString(commit.Author)
+			buffer.WriteString(",")
+			buffer.WriteString(commit.CoAuthor)
+			buffer.WriteString(",")
+			buffer.WriteString(commit.Repo)
+			buffer.WriteString(",")
+			buffer.WriteString(commit.Description)
+			buffer.WriteString("\n")
+		}
+	}
+
+	fmt.Printf(buffer.String())
+	ioutil.WriteFile("work/result_log.csv", buffer.Bytes(), 0644)
+}
+
 func main() {
 	fmt.Printf("Reading Setting File: setting.yml\n")
 	setting, err := ReadSettingFile("setting.yml")
 	if err != nil { panic(err) }
-	
-	var result map[string]map[string]int = make(map[string]map[string]int)
+
+	var count_result map[string]map[string]int = make(map[string]map[string]int)
+	var log_result map[string][]GitCommit = make(map[string][]GitCommit)
+
 	for _, contributor := range setting.Contributors {
-		result[contributor.Name] = make(map[string]int)
-	} 
-	
+		count_result[contributor.Name] = make(map[string]int)
+		log_result[contributor.Name] = make([]GitCommit, 0)
+	}
+
 	fmt.Printf("Fetching History\n")
 	var wg sync.WaitGroup
+	// var log_buffer bytes.Buffer
 	for _, repo := range setting.Repositories {
 		wg.Add(1)
 		go func(repo1 Repository){
 			defer wg.Done()
-			
+
 			fetch_error := fetchSource(repo1)
 			if fetch_error != nil {
 				panic(fetch_error)
 			}
-			
-			var file_path string = "work/" + repo1.Name + "_log.txt" 
-			var repo_counts map[string]int = CountCommits(file_path, repo1.Name, setting)
-			
-			for _, contributor := range setting.Contributors {
-				result[contributor.Name][repo1.Name] = repo_counts[contributor.Name]
+
+			var file_path string = "work/" + repo1.Name + "_log.txt"
+			inFile, err := os.Open(file_path)
+			if err != nil { panic(err) }
+			defer inFile.Close()
+
+			scanner := bufio.NewScanner(inFile)
+			var commits []GitCommit = ReadCommit(scanner, repo1.Name)
+			for _, commit := range commits {
+				// When Author and CoAuthor are both EMC, only counts as 1
+				isEmcCommit, contributorName := IsEmcCommit(commit, setting.Contributors)
+				if isEmcCommit {
+					count_result[contributorName][repo1.Name] += 1
+					log_result[contributorName] = append(log_result[contributorName], commit)
+				}
 			}
+
+
+
+			// var repo_counts map[string]int = CountCommits(file_path, repo1.Name, setting, log_buffer)
+			//
+			// for _, contributor := range setting.Contributors {
+			// 	result[contributor.Name][repo1.Name] = repo_counts[contributor.Name]
+			// }
 		}(repo)
 	}
-	
+
 	wg.Wait()
-	CreateOutputFile(setting, result)
+	CreateLogOutputFile(setting, log_result)
+	CreateOutputFile(setting, count_result)
 }
--- src/commit_count_test.go
+++ src/commit_count_test.go
@@ -3,11 +3,13 @@ package main
 import (
     "testing"
     "github.com/stretchr/testify/assert"
+    "strings"
+    "bufio"
 )
 
 var test_data = `
 ---
-repositories: 
+repositories:
 - name: Bosh
   url: some_url
 contributors:
@@ -16,26 +18,103 @@ contributors:
 
 func TestReadSetting(t *testing.T) {
 	result, err := UnmarshalYaml([]byte(test_data))
-	
+
 	assert.Equal(t, nil, err)
-	
+
 	assert.Equal(t, 1, len(result.Repositories))
 	assert.Equal(t, "Bosh", result.Repositories[0].Name)
 	assert.Equal(t, "some_url", result.Repositories[0].Url)
-	
+
 	assert.Equal(t, 1, len(result.Contributors))
 	assert.Equal(t, "Victor Fong", result.Contributors[0].Name)
 }
 
 func TestReadSettingFile(t *testing.T) {
 	result, err := ReadSettingFile("test_setting.yml")
-	
+
 	assert.Equal(t, nil, err)
-	
+
 	assert.Equal(t, 2, len(result.Repositories))
 	assert.Equal(t, "Bosh", result.Repositories[0].Name)
 	assert.Equal(t, "some_url", result.Repositories[0].Url)
-	
+
 	assert.Equal(t, 2, len(result.Contributors))
 	assert.Equal(t, "Victor Fong", result.Contributors[0].Name)
+}
+
+func TestGetFirstWord(t *testing.T) {
+  assert.Equal(t, "Authors:", GetFirstWord("Authors: Victor Fong"))
+  assert.Equal(t, "", GetFirstWord(" "))
+  assert.Equal(t, "", GetFirstWord(""))
+}
+
+var test_commit = `
+Author: Maria Shaldibina <mshaldibina@pivotal.io>
+Date:   Thu Oct 15 09:43:35 2015 -0700
+
+    Merge branch 'master' into hotfix-postgres
+
+    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>
+
+commit 7ce9e8b628034446c28b4955863386fbf4aa8c1d
+Author: Devin Fallak <dfallak@pivotal.io>
+Date:   Wed Oct 14 15:44:34 2015 -0400
+
+    Update README.md
+
+
+`
+
+func TestReadCommit(t *testing.T){
+  scanner := bufio.NewScanner(strings.NewReader(test_commit))
+  var gitCommits []GitCommit = ReadCommit(scanner, "repo1")
+  assert.Equal(t, 2, len(gitCommits))
+
+  assert.Equal(t, "Maria Shaldibina", gitCommits[0].Author)
+  assert.Equal(t, "Merge branch 'master' into hotfix-postgres", gitCommits[0].Description)
+  assert.Equal(t, "repo1", gitCommits[0].Repo)
+
+  assert.Equal(t, "Devin Fallak", gitCommits[1].Author)
+  assert.Equal(t, "Update README.md", gitCommits[1].Description)
+}
+
+func TestGetCoAuthor(t *testing.T) {
+  var testString = "    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>"
+  assert.Equal(t, "Tyler Schultz", GetCoAuthor(testString))
+}
+
+func TestGetAuthor(t *testing.T) {
+  var testString = "Author: Devin Fallak <dfallak@pivotal.io>"
+  assert.Equal(t, "Devin Fallak", GetAuthor(testString))
+}
+
+var test_commit2 = `
+Author: Victor Fong <victor.fong@emc.com>
+Date:   Thu Oct 15 09:43:35 2015 -0700
+
+    Merge branch 'master' into hotfix-postgres
+
+    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>
+`
+
+func TestIsEmcCommit(t *testing.T) {
+  scanner := bufio.NewScanner(strings.NewReader(test_commit2))
+  var gitCommits []GitCommit = ReadCommit(scanner, "repo1")
+  assert.Equal(t, 1, len(gitCommits))
+
+  setting, _ := UnmarshalYaml([]byte(test_data))
+  isEmcCommit, name := IsEmcCommit(gitCommits[0], setting.Contributors)
+
+  assert.Equal(t, true, isEmcCommit)
+  assert.Equal(t, "Victor Fong", name)
+
+}
+
+func TestIsTwoAuthorPattern(t *testing.T) {
+  var testString = "Author: Chris Piraino and Yu Zhang <cpiraino@pivotal.io>"
+  result, author1, author2 := IsTwoAuthorPattern(testString)
+
+  assert.Equal(t, true, result)
+  assert.Equal(t, "Chris Piraino", author1)
+  assert.Equal(t, "Yu Zhang", author2)
 }
